790399a3cfc050d4b40741bc241351552d95d437
Lucas Gauna <lucas794@hotmail.com>
2021-09-15 23:29:14 -0400

    learning variables

---
+++ variables/learningVariables.scala
@@ -0,0 +1,9 @@
+object learningVariables extends App {
+    val x = 1 //imutable
+    var y = 1 //mutable
+
+    val numberOne = 1 // val numberOne : Int = 1
+    val aString = "a string" // val aString : String = "a string"
+    val person = new Person("Regina") // val person: Person = new Person("Regina")
+
+}
